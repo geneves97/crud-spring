b4b35c4be58c5f80797d745a789644d6aae9866a
George Neves <georgeneves.dev@gmail.com>
2024-08-20 06:56:31 -0300

    feat: create CourseController

---
+++ src/main/java/com/george/controller/CourseController.java
@@ -0,0 +1,19 @@
+package com.george.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+
+    @GetMapping
+    public List<object> list(){
+        return null;
+    }
+
+
+}
