9442b3e9c506f2492b45fa5fe9e7e5f7a6b3427e
George Neves <georgeneves.dev@gmail.com>
2024-08-20 07:14:28 -0300

    feat: create CourseRepository.

---
+++ src/main/java/com/george/repository/CourseRepository.java
@@ -0,0 +1,10 @@
+package com.george.repository;
+
+import com.george.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+}
